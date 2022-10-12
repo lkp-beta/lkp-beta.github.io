7a5b429f986be64678eac576f49d02dc2f5fd4f4
Will Hack <will.j.hack@gmail.com>
2022-10-12 15:05:35 -0400

    Add all top level routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+import Page from './Page';
+
+import {
+  About,
+  Bands,
+  Contact,
+  Home,
+  Specialty,
+  Weddings,
+  Top,
+  Classical,
+  Jazz,
+  World,
+  Swing,
+} from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Page>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="bands-orchestras" element={<Bands />} />
+          <Route path="specialty-entertainment" element={<Specialty />} />
+          <Route path="weddings" element={<Weddings />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="top-40s" element={<Top />} />
+          <Route path="classical" element={<Classical />} />
+          <Route path="jazz" element={<Jazz />} />
+          <Route path="world" element={<World />} />
+          <Route path="swing" element={<Swing />} />
+        </Routes>
+      </Page>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
