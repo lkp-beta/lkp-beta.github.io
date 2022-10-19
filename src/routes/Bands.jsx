4fd419997f5f3659157096f69386e3e5cdea857b
Will Hack <will.j.hack@gmail.com>
2022-10-19 12:17:40 -0400

    Updates to media player

--- src/routes/Bands.jsx
+++ src/routes/Bands.jsx
@@ -1,7 +1,42 @@
+import bandsImg from '/images/bands-orchestras-pg-collage.jpg';
 export default function Bands() {
   return (
     <main>
       <h1>Bands & Orchestras</h1>
+      <section style={{ display: 'flex' }}>
+        <img src={bandsImg} alt="" />
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          Click below to hear music samples:
+          <a href="top-40s">Contemporary/Disco/R&B</a>
+          <a href="classical">Classical</a>
+          <a href="jazz">Jazz</a>
+          <a href="world">Latin & Ethnic</a>
+          <a href="swing">Swing/Pop</a>
+        </div>
+      </section>
+      <article>
+        <p>
+          Live music adds personal warmth to any event, and Larry King Productions provides
+          refreshing and innovative bands and orchestras for any occasion at nearly any budget
+          level.
+        </p>
+        <p>
+          Look to us for any type of band or orchestra. The <span>LARRY KING ORCHESTRA</span> offers
+          the perfect blend of the quintessential New York Jazz/Swing sound with a wide range of
+          hot, contemporary dance music and classic rock, ideally suited for weddings and dinner
+          dances.
+        </p>
+        <p>
+          Our <span>DOWNTOWN</span> and <span>SWINGOUTNY</span> bands also perform a variety of
+          popular contemporary and classic dance music.
+        </p>
+        <p>
+          You may want to consider our <span>KINGS OF LATIN</span> band featuring traditional and
+          contemporary salsa, merengue, and other styles of Latin music. Or, choose from many other{' '}
+          <a href="/specialty-entertainment">specialty bands and orchestras</a>, including Klezmer
+          bands, R&B groups and disco ensembles.
+        </p>
+      </article>
     </main>
   );
 }
--- src/routes/bands/Top.jsx
+++ src/routes/bands/Top.jsx
@@ -1,3 +1,93 @@
+import { useRef } from 'react';
+import topBanner from '/images/contemporary-collage.jpg';
+import discoMedley1 from '/music/top/Disco_Medley_1.mp3';
+import { useState } from 'react';
+
+import { Transition } from 'react-transition-group';
+/* eslint-disable jsx-a11y/media-has-caption */
+
+const useTrackSelection = (ref, setTrack) => {
+  const TrackSelection = ({ src, title }) => (
+    <button
+      style={{ textAlign: 'left' }}
+      onClick={() => {
+        ref.src = src;
+        ref.current.load();
+        ref.current.play();
+        setTrack(title);
+      }}
+    >
+      {title}
+    </button>
+  );
+
+  return TrackSelection;
+};
+
 export default function Top() {
-  return <main></main>;
+  const ref = useRef(null);
+  const [track, setTrack] = useState();
+
+  // const { MusicBox, TrackSelection } = useTrackButton(playerRef);
+
+  // const TrackSelection = ({ src, title }) => (
+  //   <button
+  //     style={{ textAlign: 'left' }}
+  //     onClick={() => {
+  //       ref.src = src;
+  //       ref.current.load();
+  //       ref.current.play();
+  //       setTrack(title);
+  //     }}
+  //   >
+  //     {title}
+  //   </button>
+  // );
+
+  const TrackSelection = useTrackSelection(ref, setTrack);
+
+  return (
+    <main>
+      <img alt="" src={topBanner} />
+      <a href="bands-orchestras">| Bands & Orchestras</a>
+      <h1>Contemporary | Disco | R&B</h1>
+      <span>Click below to hear music samples:</span>
+      <Transition in={!!track} timeout={450}>
+        {state => (
+          <div
+            style={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              transition: `opacity ${450}ms ease-in-out`,
+              opacity: state === 'entered' || state === 'entering' ? 1 : 0,
+            }}
+          >
+            <h3>{track}</h3>
+            <hr className="linearHr" />
+            <audio
+              controls
+              controlsList="nodownload"
+              ref={ref}
+              style={{
+                width: '85%',
+                margin: '24px 0',
+                alignSelf: 'center',
+              }}
+            >
+              <source src={ref.src} type="audio/mpeg" />
+              Your browser does not support the audio element.
+            </audio>
+            <hr className="linearHr" style={{ marginBottom: '20px' }} />
+          </div>
+        )}
+      </Transition>
+      <TrackSelection title="Disco Medley 1" src={discoMedley1} />
+      {/* <TrackSelection title="Disco Medley 2" src={discoMedley2} /> */}
+      {/* <TrackSelection title="Respect" /> */}
+      {/* <TrackSelection title="Barry White Medley" /> */}
+      {/* <TrackSelection title="Don't Know Why" /> */}
+      {/* <TrackSelection title="Sexual Healing" /> */}
+    </main>
+  );
 }
